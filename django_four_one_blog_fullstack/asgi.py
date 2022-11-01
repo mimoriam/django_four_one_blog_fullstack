afe9c0c105438fe5dec6a4303810a534e8698c9d
Mimo <mimoriam420@gmail.com>
2022-11-01 12:22:39 +0500

    Initial commit

---
+++ django_four_one_blog_fullstack/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for django_four_one_blog_fullstack project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "django_four_one_blog_fullstack.settings"
+)
+
+application = get_asgi_application()
