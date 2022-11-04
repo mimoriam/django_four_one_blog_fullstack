ffe47b69b05fea8b79c35dcdefc28c6cd7e77caf
Mimo <mimoriam420@gmail.com>
2022-11-04 16:11:29 +0500

    Commit #10 - Converted post_list function to class-based view (PostListView)

--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 
 # GET ALL:
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        # If page_number is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        # If page_number is not an integer deliver the first page
-        posts = paginator.page(1)
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
-
-
-# GET ONE:
-# def post_detail(request, id):
-#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-#
-#     return render(request,
-#                   'blog/post/detail.html',
-#                   {'post': post})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month,
-                             publis__day=day)
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
